077fd436857592da6743823d1fae84aefaaaef4f
dieptv1999 <diep.tv1999@gmail.com>
2024-06-23 15:08:20 +0700

    .

--- src/theme.config.js
+++ src/theme.config.js
@@ -1,75 +1,81 @@
-import { useConfig } from 'nextra-theme-docs'
+import {useConfig} from 'nextra-theme-docs'
 
 /* eslint sort-keys: error */
 /**
  * @type {import('nextra-theme-docs').DocsThemeConfig}
  */
 export default {
-  // banner: {
-  //   key: 'Nextra 2',
-  //   text: 'Nextra 2 Alpha'
-  // },
-  chat: {
-    link: 'https://discord.gg/hEM84NMkRv' // Next.js discord server,
-  },
-  docsRepositoryBase:
-    'https://github.com/tecklens/wolfx.docs',
-  editLink: {
-    text: 'Edit this page on GitHub'
-  },
-  faviconGlyph: '✦',
-  useNextSeoProps() {
-    const { frontMatter } = useConfig()
-    return {
-      additionalLinkTags: [
-        {
-          href: '/apple-icon-180x180.png',
-          rel: 'apple-touch-icon',
-          sizes: '180x180'
-        },
-        {
-          href: '/android-icon-192x192.png',
-          rel: 'icon',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          href: '/favicon-96x96.png',
-          rel: 'icon',
-          sizes: '96x96',
-          type: 'image/png'
-        },
-        {
-          href: '/favicon-32x32.png',
-          rel: 'icon',
-          sizes: '32x32',
-          type: 'image/png'
-        },
-        {
-          href: '/favicon-16x16.png',
-          rel: 'icon',
-          sizes: '16x16',
-          type: 'image/png'
+    chat: {
+        icon: (
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <path
+                    d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+            </svg>
+        ),
+        link: 'https://github.com/tecklens', // Next.js discord server,
+    },
+    docsRepositoryBase:
+        'https://github.com/tecklens/wolfx.docs',
+    editLink: {
+        text: 'Edit this page on GitHub'
+    },
+    faviconGlyph: '✦',
+    footer: {
+        text: 'MIT 2024 © WOLFX.'
+    },
+    logo: <span style={{fontWeight: '700'}}>Wolfx Documentation</span>,
+    useNextSeoProps() {
+        const {frontMatter} = useConfig()
+        return {
+            additionalLinkTags: [
+                {
+                    href: '/apple-icon-180x180.png',
+                    rel: 'apple-touch-icon',
+                    sizes: '180x180'
+                },
+                {
+                    href: '/android-icon-192x192.png',
+                    rel: 'icon',
+                    sizes: '192x192',
+                    type: 'image/png'
+                },
+                {
+                    href: '/favicon-96x96.png',
+                    rel: 'icon',
+                    sizes: '96x96',
+                    type: 'image/png'
+                },
+                {
+                    href: '/favicon-32x32.png',
+                    rel: 'icon',
+                    sizes: '32x32',
+                    type: 'image/png'
+                },
+                {
+                    href: '/favicon-16x16.png',
+                    rel: 'icon',
+                    sizes: '16x16',
+                    type: 'image/png'
+                }
+            ],
+            additionalMetaTags: [
+                {content: 'en', httpEquiv: 'Content-Language'},
+                {content: 'WOLFX', name: 'apple-mobile-web-app-title'},
+                {content: '#fff', name: 'msapplication-TileColor'},
+                {content: '/ms-icon-144x144.png', name: 'msapplication-TileImage'}
+            ],
+            description:
+                frontMatter.description || 'WOLFX: the Next.js site builder',
+            openGraph: {
+                images: [
+                    {url: frontMatter.image || 'https://docs.wolfx.app/og.png'}
+                ]
+            },
+            titleTemplate: '%s – WOLFX',
+            twitter: {
+                cardType: 'summary_large_image',
+                site: 'https://docs.wolfx.app'
+            }
         }
-      ],
-      additionalMetaTags: [
-        { content: 'en', httpEquiv: 'Content-Language' },
-        { content: 'WOLFX', name: 'apple-mobile-web-app-title' },
-        { content: '#fff', name: 'msapplication-TileColor' },
-        { content: '/ms-icon-144x144.png', name: 'msapplication-TileImage' }
-      ],
-      description:
-        frontMatter.description || 'WOLFX: the Next.js site builder',
-      openGraph: {
-        images: [
-          { url: frontMatter.image || 'https://docs.wolfx.app/og.png' }
-        ]
-      },
-      titleTemplate: '%s – WOLFX',
-      twitter: {
-        cardType: 'summary_large_image',
-        site: 'https://docs.wolfx.app'
-      }
-    }
-  }
+    },
 }
