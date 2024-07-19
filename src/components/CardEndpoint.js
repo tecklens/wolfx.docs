33b40debcb2a7a969632c14090340331c602d6de
Trần Văn Điệp <38648881+dieptv1999@users.noreply.github.com>
2024-07-19 15:19:49 +0700

    Update CardEndpoint.js

--- src/components/CardEndpoint.js
+++ src/components/CardEndpoint.js
@@ -12,7 +12,7 @@ export default function CardEndpoint({title = "ENDPOINTS", url = '', method = 'G
   }
 
   return (
-    <div className={'nx-border dark:nx-border-neutral-800 nx-flex nx-flex-col nx-rounded-lg nx-text-sm nx-my-2'} style={{minWidth: '280px', width: '100%'}}>
+    <div className={'nx-border dark:nx-border-neutral-800 nx-flex nx-flex-col nx-rounded-lg nx-text-sm nx-my-2 nx-overflow-hidden'} style={{minWidth: '280px', width: '100%'}}>
       <div className={'nx-px-2 nx-font-semibold nx-py-1 nx-bg-gray-200 dark:nx-bg-gray-50/10 border-b'}>{title}</div>
       <div className={'nx-flex nx-px-2 nx-py-1 nx-bg-gray-100 dark:nx-bg-black/60'}>
         <div className={'nx-font-semibold'} style={{color: getColorMethod()}}>{method?.toUpperCase()}</div>
